8c8ad20861bb5fa58c0bf3a70dfafbb06728680c
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-04 14:20:53 +0100

    Complete stretch goals - async timer and increment if odd

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,34 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
+export const INCREMENT_IF_ODD = 'INCREMENT_IF_ODD';
+
 class Counter extends Component {
   incrementIfOdd = () => {
-    // Stretch Problem: Implement an increment function that
-    // only increments if the counter value is odd
+    if (this.props.count % 2 === 0) {
+      this.props.increment();
+    }
   };
 
   incrementAsync = () => {
-    // Stretch Problem: Implement an increment function that
-    // increments after waiting for one second
+    setTimeout(this.props.increment, 1000);
   };
 
   render() {
-    // Fill in the two button onClick methods
-    // Upon clicking these buttons, the count
-    // should decrement or increment accordingly
     return (
       <p>
         Clicked: {this.props.count} times
         <button onClick={this.props.increment}>+</button>
         <button onClick={this.props.decrement}>-</button>
-        {/* Uncomment these button tags if you got
-                around to implementing the extra credit functions */}
-        {/* <button onClick={this.incrementIfOdd}>
-                    Increment if odd
-                </button>
-                <button onClick={this.incrementAsync}>
-                    Increment async
-                </button>  */}
+        <button onClick={this.incrementIfOdd}>Increment if odd</button>
+        <button onClick={this.incrementAsync}>Increment async</button>
       </p>
     );
   }
